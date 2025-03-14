b2a815089b8868a92fc7d0c3d00b712bfa55390f
Hugo Kocers <101929960+HugoK2007@users.noreply.github.com>
2025-03-14 13:02:19 +0200

    Update Tetris.java

--- tetris/src/main/java/com/tetris/Tetris.java
+++ tetris/src/main/java/com/tetris/Tetris.java
@@ -1,69 +1,43 @@
 package com.tetris;
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Timer;
+import java.util.TimerTask;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 
-public class Form {
-    Rectangle a;
-    Rectangle b;
-    Rectangle c;
-    Rectangle d;
-    Color color;
-    private String name;
-    public int form = 1;
+
+public class Tetris extends com.tetris {
+
+    public static final int MOVE = 25;
+    public static final int SIZE = 25;
+    public static int XMAX = SIZE = 12;
+    public static int YMAX = SIZE = 24;
+    public static int [][] MESH = new int [XMAX/SIZE][YMAX/SIZE];
+    private static Pane groupe = new Pane();
+    private static Form object;
+    private static Scene scene = new Scene(groupe, XMAX + 150, YMAX);
+    public static int score = 0;
+    private static boolean game = true;
+    private static Form nextObj = Controller.makeRect();
+    private static int LinesNo = 0;
 
 
-    public Form (Rectangle a, Rectangle b, Rectangle c, Rectangle d) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
-        this.d = d;
+
+    @Override
+    public void start(Stage arg0) throws Exception {
+
     }
-
-    public Form (Rectangle a, Rectangle b, Rectangle c, Rectangle d, String name) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
-        this.d = d;
-        this.name = name;
     
-
-    switch (name) {
-        case "j":
-          color = Color.SLATEGRAY;
-        case "l":
-          color = Color.DARKGOLDENROD;
-        case "o":
-          color = Color.INDIANRED;
-        case "s":
-          color = Color.INDIGO;
-        case "t":
-          color = Color.FORESTGREEN;
-        case "z":
-          color = Color.PINK;
-        
-    }
-
-    this.a.setFill(color);
-    this.b.setFill(color);
-    this.c.setFill(color);
-    this.d.setFill(color);
-    }
-
-
-
-    public String getName() {
-        return name;
-    }
-    public void changeForm() {
-        if (form != 1 ) {
-            form++;
-        } else {
-            form = 1;
-        }
-    }
-
-
-
 }
